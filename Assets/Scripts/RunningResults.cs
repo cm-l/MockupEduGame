02b12e155b2a3a02b3b3a40bb109a62c6baed3e5
Patryk Garwol <patrykgarwol2000@wp.pl>
2023-04-21 20:27:43 +0200

    Podliczanie kasy + wyświetlenie na canvasie (Running)

--- Assets/Scripts/RunningResults.cs
+++ Assets/Scripts/RunningResults.cs
@@ -12,11 +12,19 @@ public class RunningResults : MonoBehaviour
     private TextMeshProUGUI multiplicationScoreBox;
     private TextMeshProUGUI divisionScoreBox;
     private TextMeshProUGUI totalPointsScoreBox;
+    private TextMeshProUGUI moneyEarnedBox;
 
     [SerializeField] private static int additionScore = 0;
     [SerializeField] private static int subtractionScore = 0;
     [SerializeField] private static int multiplicationScore = 0;
     [SerializeField] private static int divisionScore = 0;
+
+    [SerializeField] private static int highestPossibleScore = 10;
+    [SerializeField] private static int earnedPoints = 0;
+    [SerializeField] private static float percentOfSuccessfulRun = 0f;
+
+    public static int moneyEarned;
+    public static int limitOfMoneyToEarn = 70;
     
     void Start() {
         additionScoreBox = GameObject.Find("AdditionScoreBox").GetComponent<TextMeshProUGUI>();
@@ -24,6 +32,7 @@ public class RunningResults : MonoBehaviour
         multiplicationScoreBox = GameObject.Find("MultiplicationScoreBox").GetComponent<TextMeshProUGUI>();
         divisionScoreBox = GameObject.Find("DivisionScoreBox").GetComponent<TextMeshProUGUI>();
         totalPointsScoreBox = GameObject.Find("SummaryScoreBox").GetComponent<TextMeshProUGUI>();
+        moneyEarnedBox = GameObject.Find("MoneyEarnedBox").GetComponent<TextMeshProUGUI>();
 
         additionScoreBox.text = additionScore.ToString();
         subtractionScoreBox.text = subtractionScore.ToString();
@@ -31,43 +40,59 @@ public class RunningResults : MonoBehaviour
         divisionScoreBox.text = divisionScore.ToString();
         totalPointsScoreBox.text = TMPController.getScore().ToString();
 
-        Timer timer = new Timer(state =>
-        {
-            // Code to execute after 1 second
-            Console.WriteLine("One second has passed.");
-        }, null, 1000, Timeout.Infinite);
+        percentOfSuccessfulRun = (float)earnedPoints / highestPossibleScore;
+        Debug.Log("Success rate: " + percentOfSuccessfulRun);
+        moneyEarned = CalculateMoneyEarned();
+       
+        moneyEarnedBox.text = "You earned $" + moneyEarned + "!";
+
     }
 
     public static void AdditionScoreUp() {
         additionScore++;
+        earnedPoints++;
     }
 
     public static void AdditionScoreDown() {
         additionScore--;
+        earnedPoints--;
     }
 
     public static void SubtractionScoreUp() {
         subtractionScore++;
+        earnedPoints++;
     }
 
     public static void SubtractionScoreDown() {
         subtractionScore--;
+        earnedPoints--;
     }
 
     public static void MultiplicationScoreUp() {
         multiplicationScore++;
+        earnedPoints++;
     }
 
     public static void MultiplicationScoreDown() {
         multiplicationScore--;
+        earnedPoints--;
     }
 
     public static void DivisionScoreUp() {
         divisionScore++;
+        earnedPoints++;
     }
 
     public static void DivisionScoreDown() {
         divisionScore--;
+        earnedPoints--;
+    }
+
+    private static int CalculateMoneyEarned() {
+        if (percentOfSuccessfulRun <= 0)
+            return 0;
+        else
+            return (int) percentOfSuccessfulRun * limitOfMoneyToEarn;
     }
 
     public static void ResetAllScores() {
